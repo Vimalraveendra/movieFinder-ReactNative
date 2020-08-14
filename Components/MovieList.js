dc8987c064173eef1ed112ccbdd11aeaa27e67f4
vimal Raveendran <vimalraveendra@gmail.com>
2020-08-14 19:55:18 +0200

    Added Close button to PopUpWindow component & updated App component with error handling

--- Components/MovieList.js
+++ Components/MovieList.js
@@ -12,21 +12,20 @@ const MovieList = ({moviesList, openPopUpWindow}) => {
   return (
     <View style={styles.movies}>
       <ScrollView>
-        {moviesList.length > 0 &&
-          moviesList.map((movie) => (
-            <TouchableHighlight
-              key={movie.imdbID}
-              onPress={() => openPopUpWindow(movie.imdbID)}>
-              <View style={styles.movie}>
-                <Image
-                  source={{uri: movie.Poster}}
-                  style={styles.image}
-                  resizeMode="cover"
-                />
-                <Text style={styles.heading}>{movie.Title}</Text>
-              </View>
-            </TouchableHighlight>
-          ))}
+        {moviesList.map((movie) => (
+          <TouchableHighlight
+            key={movie.imdbID}
+            onPress={() => openPopUpWindow(movie.imdbID)}>
+            <View style={styles.movie}>
+              <Image
+                source={{uri: movie.Poster}}
+                style={styles.image}
+                resizeMode="cover"
+              />
+              <Text style={styles.heading}>{movie.Title}</Text>
+            </View>
+          </TouchableHighlight>
+        ))}
       </ScrollView>
     </View>
   );
@@ -42,16 +41,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     width: '100%',
   },
+  image: {
+    width: '100%',
+    height: 300,
+  },
   heading: {
     color: '#fff',
     fontSize: 20,
     fontWeight: '700',
     padding: 20,
     backgroundColor: '#445565',
-  },
-  image: {
-    width: '100%',
-    height: 300,
   },
 });
 
